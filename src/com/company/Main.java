8b0cb99d3a83a8876169fb128e57bb788aec5d4d
trini7y <okekedesmond71@gmail.com>
2020-01-30 16:58:40 +0100

    finished up the code and added some features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,67 +1,90 @@
 package com.company;
 import java.util.Scanner;
 public class Main {
-    public  Scanner getInput = new Scanner(System.in);
-    public  ControlPanel bank = new ControlPanel();
+    public static Scanner getInput = new Scanner(System.in);
+    public static ControlPanel bank = new ControlPanel();
+    public static int password, masterPass, acctNumber;
+    public static double amount;
+    public static String name;
+    public static  String  bankName;
 
-    public  int password, masterPass, acctNumber;
-    public  double amount;
-    public  String name;
-    String bankName;
-
-
-    public  void main(String[] args) {
-            System.out.println("Please Enter Your Name:");
+    public static void main(String[] args) {
+            System.out.print("Please Enter Your Name:");
             name = getInput.next();
-            System.out.println("Enter your password:");
+            System.out.print("Enter your password:");
             password = getInput.nextInt();
             while(true){
-                password = getInput.nextInt();
                 if(password < 9999){
                     bank.setPassword(password);
                     break;
                 }else{
-                    System.out.println("Password most not exceed 4");
+                    System.out.print("Enter your password:");
+                    password = getInput.nextInt();
                 }
             }
             System.out.println("Welcome " + name +
-                    "What do you want to do today" +
-                    "1) Withdrawal "+
-                    "2) Transfer" +
-                    "3) Check Balance"+
-                    "4");
-            String
-
-            
+                    "\nWhat do you want to do today\n" +
+                    "1) Withdrawal \n"+
+                    "2) Transfer \n" +
+                    "3) Check Balance \n"+
+                    "4) Exit");
+            String options;
+            options = getInput.next();
+            int counter =0;
+            while(options != "4"){
+                switch (options){
+                    case "1":
+                        withDraw();
+                        break;
+                    case "2":
+                        transfer();
+                        break;
+                    case "3":
+                        bankerBalance();
+                        break;
+                    default: System.out.println("You haven't chosen an option");
+                        break;
+                }
+                System.out.println("Welcome " + name +
+                        "\nWhat do you want to do today\n" +
+                        "1) Withdrawal \n"+
+                        "2) Transfer \n" +
+                        "3) Check Balance \n"+
+                        "4) Exit");
+                options = getInput.next();
+                counter++;
+            }
+            if(counter != 0){
+                System.out.println("Thank you for Banking with us" + "\n Have a nice day (-_-)");
+            }
     }
-    public void transfer(double amt){
+    public static void transfer(){
             System.out.println("Enter the receivers Account number:");
-            acctNumber = getInput.nextInt();
+            int acctNumber = getInput.nextInt();
             if(acctNumber >= 5000){
                 System.out.println("Input the correct password");
             }else{
                 System.out.println("Enter the bank:");
                 bankName = getInput.next();
                 System.out.println("Amount:");
-                amt = getInput.nextDouble();
+                double amt = getInput.nextDouble();
                 bank.setWithDrawalBankerBalance(amt);
             }
             System.out.println("\t \t Receipt");
             System.out.println("_________________________________");
             System.out.print("Transaction to the account :" + acctNumber + "\n Bank:" + bankName + "\n Amount:" +
-                    amount + " \t *** Was successful ***" + "\n \n Your current Balance is :"+ bank.getBankerBalance());
-
+                    amount + " \t *** Was successful ***" + "\n \n Your current Balance is :"+ bank.getBankerBalance() +"\n");
     }
-    public void withDraw(double amt){
+    public static void withDraw(){
             System.out.println("How much do you want to withdraw:");
             amount = getInput.nextDouble();
-            bank.setWithDrawalBankerBalance(amt);
+            bank.setWithDrawalBankerBalance(amount);
             System.out.println("Transaction Successful");
             System.out.println("\t Balance");
-            System.out.println("Your current Balance is:" + bank.getBankerBalance());
+            System.out.println("Your current Balance is:" + bank.getBankerBalance() + "\n");
     }
-    public void bankerBalance(){
-            System.out.println("Your Balance is :" + bank.getBankerBalance());
+    public static void bankerBalance(){
+            System.out.println("Your Balance is :" + bank.getBankerBalance() +"\n");
     }
 //    public void atmBalance(){
 //
